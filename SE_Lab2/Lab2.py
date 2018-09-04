430d43c4055362a20317a38d15860148a6cf6efd
Marc Karp <mkarp@mk.local>
2018-09-04 04:05:23 +0200

    added unit tests v1.0

--- SE_Lab2/Lab2.py
+++ SE_Lab2/Lab2.py
@@ -3,19 +3,27 @@ import math
 import time
 
 
-def PrimeNumbers(x):
+def generate_eratothenes_list(x):
     if type(x) is not int:
         raise TypeError("Invalid input. Only integers permitted.")
     
     if x < 2:
         raise ValueError("Prime numbers can only be generated for values greater than 1.")
+    return list(range(2, x+1))
     
-    all_numbers = list(range(2, x+1))
-    
+
+def remove_composites(all_numbers):
+      return list((value for value in all_numbers if value != -1))
+
+def PrimeNumbers(x):
+  
+    all_numbers = generate_eratothenes_list(x)
+
     # Optimization to stop at squareroot of number X
     index_of_sqrt = all_numbers.index(math.ceil(math.sqrt(x)))
     
     start_time = time.time()
+    #Sieve of Eratothenes Algorithm
     for pos in range(0, index_of_sqrt+1):
         current_number = all_numbers[pos]
         # Skip marked values 
@@ -29,9 +37,9 @@ def PrimeNumbers(x):
                    all_numbers[all_numbers.index(a)] = -1
 
     #Remove all marked values, these are all NOT prime
-    all_numbers[:] = (value for value in all_numbers if value != -1)
+    all_numbers[:] = remove_composites(all_numbers)
     
     return all_numbers
             
 
-print(PrimeNumbers(100000))
+
--- SE_Lab2/test_Lab2.py
+++ SE_Lab2/test_Lab2.py
@@ -1,1 +1,59 @@
-import unittest 
+import unittest
+import Lab2
+
+class TestLab2(unittest.TestCase):
+
+    def test_generate_eratothenes_list(self):
+        self.assertEqual(Lab2.generate_eratothenes_list(3),[2,3])
+        self.assertEqual(Lab2.generate_eratothenes_list(100),[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
+
+        #check ValueError
+        with self.assertRaises(ValueError):
+            Lab2.generate_eratothenes_list(1)
+        with self.assertRaises(ValueError):
+            Lab2.generate_eratothenes_list(0) # edge case
+        with self.assertRaises(ValueError):
+            Lab2.generate_eratothenes_list(-1) # edge case
+
+        #check TypeError
+        with self.assertRaises(TypeError):
+            Lab2.PrimeNumbers(6.4) # edge case
+        with self.assertRaises(TypeError):
+            Lab2.generate_eratothenes_list('2') # edge case
+        with self.assertRaises(TypeError):
+            Lab2.generate_eratothenes_list("2") # edge case    
+        with self.assertRaises(TypeError):
+           Lab2.generate_eratothenes_list('six') # edge case
+        with self.assertRaises(TypeError):
+           Lab2.generate_eratothenes_list("six") # edge case
+
+    def test_remove_composites(self):
+        self.assertEqual(Lab2.remove_composites([2,3,-1,5,-1,7]),[2,3,5,7])
+
+    def test_PrimeNumbers(self):
+        self.assertEqual(Lab2.PrimeNumbers(100),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+        #edge case non perfect square
+        self.assertEqual(Lab2.PrimeNumbers(80),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79]) 
+        #check TypeError
+        with self.assertRaises(TypeError):
+           Lab2.PrimeNumbers('100') # edge case
+        with self.assertRaises(TypeError):
+           Lab2.PrimeNumbers("100") # edge case    
+        with self.assertRaises(TypeError):
+           Lab2.PrimeNumbers('six') # edge case
+        with self.assertRaises(TypeError):
+           Lab2.PrimeNumbers("six") # edge case
+        with self.assertRaises(TypeError):
+           Lab2.PrimeNumbers(6.4) # decimal edge case
+
+        #check ValueError as defined by a prime number is greater than 2
+        with self.assertRaises(ValueError):
+            Lab2.PrimeNumbers(1)
+        with self.assertRaises(ValueError):
+            Lab2.PrimeNumbers(0) # edge case
+        with self.assertRaises(ValueError):
+            Lab2.PrimeNumbers(-1) # edge case
+
+if __name__ == '__main__':
+    unittest.main()
+
