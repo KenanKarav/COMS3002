72c53dc9d3997a2dfaddb8b6d477dc5e22fbf7d9
KenanKarav <k.karavoussanos@gmail.com>
2018-09-04 13:53:31 +0200

    in test_Lab2.py added output datatype checking test cases and removed redundant test cases also modified import statement for Lab2 to work with the fully cloned repo; In TestCases.txt expanded scenario description and wrote high-level description of test-program

--- SE_Lab2/test_Lab2.py
+++ SE_Lab2/test_Lab2.py
@@ -1,11 +1,18 @@
 import unittest
-import Lab2
+import COMS3002.SE_Lab2.Lab2 as Lab2
 
 class TestLab2(unittest.TestCase):
 
     def test_generate_eratothenes_list(self):
+        self.assertIsInstance(Lab2.generate_eratothenes_list(3), list)
         self.assertEqual(Lab2.generate_eratothenes_list(3),[2,3])
-        self.assertEqual(Lab2.generate_eratothenes_list(100),[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
+        self.assertEqual(Lab2.generate_eratothenes_list(100),[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                                                              18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                                                              32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+                                                              46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                                                              60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73,
+                                                              74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87,
+                                                              88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
 
         #check ValueError
         with self.assertRaises(ValueError):
@@ -28,27 +35,29 @@ class TestLab2(unittest.TestCase):
            Lab2.generate_eratothenes_list("six") # edge case
 
     def test_remove_composites(self):
+        self.assertIsInstance(Lab2.remove_composites([2,3,-1,5,-1,7]), list)
         self.assertEqual(Lab2.remove_composites([2,3,-1,5,-1,7]),[2,3,5,7])
 
+
     def test_PrimeNumbers(self):
+
+        #testing return type
+        self.assertIsInstance(Lab2.PrimeNumbers(100), list)
         self.assertEqual(Lab2.PrimeNumbers(100),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
         #edge case non perfect square
+        self.assertIsInstance(Lab2.PrimeNumbers(80), list)
         self.assertEqual(Lab2.PrimeNumbers(80),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79]) 
         #check TypeError
         with self.assertRaises(TypeError):
-           Lab2.PrimeNumbers('100') # edge case
+           Lab2.PrimeNumbers('100') # valid integer number as a string
         with self.assertRaises(TypeError):
-           Lab2.PrimeNumbers("100") # edge case    
+           Lab2.PrimeNumbers('six') # number word as a string
         with self.assertRaises(TypeError):
-           Lab2.PrimeNumbers('six') # edge case
-        with self.assertRaises(TypeError):
-           Lab2.PrimeNumbers("six") # edge case
-        with self.assertRaises(TypeError):
-           Lab2.PrimeNumbers(6.4) # decimal edge case
+           Lab2.PrimeNumbers(6.4) # valid positive number but not an integer
 
         #check ValueError as defined by a prime number is greater than 2
         with self.assertRaises(ValueError):
-            Lab2.PrimeNumbers(1)
+            Lab2.PrimeNumbers(1) # edge case
         with self.assertRaises(ValueError):
             Lab2.PrimeNumbers(0) # edge case
         with self.assertRaises(ValueError):
