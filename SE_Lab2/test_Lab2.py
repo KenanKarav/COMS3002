767c136025893e8db39c43fbb2d60757e1c659fe
preshen-goobiah <1355880@students.wits.ac.za>
2018-09-10 01:45:55 +0200

    Described PrimeNumbers test cases

--- SE_Lab2/test_Lab2.py
+++ SE_Lab2/test_Lab2.py
@@ -66,27 +66,31 @@ class TestLab2(unittest.TestCase):
         self.assertEqual(Lab2.remove_composites([2,3,-1,5,-1,7])["exit_message"],
                          "Exited Normally")
 
-    
-
 
 
     def test_PrimeNumbers(self):
 
-        #testing return type
+        #Test Correct output
+        #Input: 1000
+        #Output: All primes under 1000
         self.assertIsInstance(Lab2.PrimeNumbers(1000)["list"], list)
         self.assertEqual(Lab2.PrimeNumbers(1000)["list"],
                         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997])
         self.assertEqual(Lab2.PrimeNumbers(1000)["exit_code"], 1)
         self.assertEqual(Lab2.PrimeNumbers(1000)["exit_message"], "Exited Normally")
 
-        #edge case non perfect square
+        #Test Correct Output
+        #Input: 80 (Square root of 80 is not a perfect square, program could crash when trying to find index of the square root of 80)
+        #Output: All primes under 80
         self.assertIsInstance(Lab2.PrimeNumbers(80)["list"], list)
         self.assertEqual(Lab2.PrimeNumbers(80)["list"],[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79])
         self.assertEqual(Lab2.PrimeNumbers(80)["exit_code"], 1)
         self.assertEqual(Lab2.PrimeNumbers(80)["exit_message"], "Exited Normally")
 
 
-        #check TypeError
+        #Test Type Error
+        #Input: "100", "six", 6.4
+        #Output: Exit code -1, Exit Message: Invalid Input. Only Integers allowed
         self.assertEqual(Lab2.PrimeNumbers('100')["exit_code"], -1)
         self.assertEqual(Lab2.PrimeNumbers('100')["exit_message"], "Invalid input. Only integers allowed")
         self.assertEqual(Lab2.PrimeNumbers('six')["exit_code"], -1)
@@ -95,18 +99,16 @@ class TestLab2(unittest.TestCase):
         self.assertEqual(Lab2.PrimeNumbers(6.4)["exit_message"], "Invalid input. Only integers allowed")
 
 
-        #check ValueError as defined by a prime number is greater than 2
+        #Test Value Error
+        #Input: 1,0,-1
+        #Output: Exit code -1, Exit message - Invalid input. Input must be an integer greater than 1 to produce prime number
         self.assertEqual(Lab2.PrimeNumbers(1)["exit_code"], -1)
         self.assertEqual(Lab2.PrimeNumbers(1)["exit_message"], "Invalid input. Input must be an integer greater than 1 to produce prime number")
         self.assertEqual(Lab2.PrimeNumbers(0)["exit_code"], -1)
         self.assertEqual(Lab2.PrimeNumbers(0)["exit_message"],
                          "Invalid input. Input must be an integer greater than 1 to produce prime number")
         self.assertEqual(Lab2.PrimeNumbers(-1)["exit_code"], -1)
-        self.assertEqual(Lab2.PrimeNumbers(-1)["exit_message"],
- 
-                         
-                         
-                         "Invalid input. Input must be an integer greater than 1 to produce prime number")
+        self.assertEqual(Lab2.PrimeNumbers(-1)["exit_message"],"Invalid input. Input must be an integer greater than 1 to produce prime number")
 
 
 
