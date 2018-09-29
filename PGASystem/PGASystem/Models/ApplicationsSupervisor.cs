9f5f4ec6fdfba4f9021d8cc0ff82831c74cd9941
Marc Wits <marc@mk.local>
2018-09-30 00:00:32 +0200

    Created assigned applicants for specific supervisor service/viewModel/view and action

---
+++ PGASystem/PGASystem/Models/ApplicationsSupervisor.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PGASystemData.Models;
+
+namespace PGASystem.Models
+{
+    public class Applications
+    {
+        public IEnumerable<Application> ApplicationsAssigned { get; set; }
+    }
+}
